c948dc217b0bc8ebee4382649fbbe803ad37cdbd
handmedown453 <handmedown453@gmail.com>
2019-04-22 01:44:53 -0700

    Merge branches 'dev_dipesh' and 'development' of https://github.com/handmedown453/HandMeDown into dev_dipesh
    # Conflicts:
    #	app/src/main/java/android/project/handmedown/MainActivity.java

--- app/src/main/java/android/project/handmedown/MainActivity.java
+++ app/src/main/java/android/project/handmedown/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"Mounil", Toast.LENGTH_LONG).show();
         Toast.makeText(getApplicationContext(),"Shyam", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"Dipesh", Toast.LENGTH_LONG).show();
     }
 }
