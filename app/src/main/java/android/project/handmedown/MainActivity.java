2cf5b89615027c3c94a76a990e22486450591137
shyam6796 <shyam.kantesariya@gmail.com>
2019-04-22 01:42:24 -0700

    Merge branch 'development' of https://github.com/handmedown453/HandMeDown into dev_shyam
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/android/project/handmedown/MainActivity.java
+++ app/src/main/java/android/project/handmedown/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"Mounil", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),"Shyam", Toast.LENGTH_LONG).show();
     }
 }
