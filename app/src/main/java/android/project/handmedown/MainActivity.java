c730e77ac21fd10012aab53aff31ccb20be33183
Mounil Shah <mounil3196@gmail.com>
2019-04-25 23:25:46 -0700

    Add Navigation Bar

--- app/src/main/java/android/project/handmedown/MainActivity.java
+++ app/src/main/java/android/project/handmedown/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"Mounil", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(),"Shyam", Toast.LENGTH_LONG).show();
-        Toast.makeText(getApplicationContext(),"Dipesh", Toast.LENGTH_LONG).show();
+
     }
 }
